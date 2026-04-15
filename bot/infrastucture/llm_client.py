6bc5aca1a8d8b439ea189f9b78dc4f6afa9af4af
Ben Abramyan <3580920@mail.ru>
2026-04-15 23:04:15 +0300

    перешёл с /chat на /generate т.к. старая версия ollama, также теперь создаю full_prompt для передачи в generate

--- bot/infrastucture/llm_client.py
+++ bot/infrastucture/llm_client.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 class MathAIClient:
     def __init__(self, api_url: str, timeout: int = 30):
-        self.api_url = api_url + "/api/chat"
+        self.api_url = api_url + "/api/generate"
         self.timeout = httpx.Timeout(timeout)
         self.client = httpx.AsyncClient(timeout=self.timeout, limits=httpx.Limits(max_connections=100))
         self.headers = {
@@ -14,9 +14,17 @@ class MathAIClient:
         }
 
     async def chat_completion(self, messages: list, model: str = "qwen-2.5:3b", temperature: float = 0.) -> str:
+        full_prompt = ""
+        for msg in messages:
+            role = msg["role"]
+            content = msg["content"]
+            if role == "system":
+                full_prompt += f"{content}\n\n"
+            elif role == "user":
+                full_prompt += f"Input: {content}\n\nOutput:"
         payload = {
             "model": model,
-            "messages": messages,
+            "messages": full_prompt,
             "temperature": temperature,
             "stream": False
         }
@@ -24,7 +32,7 @@ class MathAIClient:
             response = await self.client.post(self.api_url, json=payload, headers=self.headers)
             response.raise_for_status()
             data = response.json()
-            return data['message']['content']
+            return data['response']
         except httpx.HTTPStatusError as e:
             logger.error(f"LLM HTTP error {e.response.status_code}: {e}")
             raise
