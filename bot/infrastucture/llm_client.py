356644d78eb02176601c9c35d217b6ae244c0ebb
Ben Abramyan <3580920@mail.ru>
2026-04-15 22:49:41 +0300

    trigger ci/cd

--- bot/infrastucture/llm_client.py
+++ bot/infrastucture/llm_client.py
@@ -32,5 +32,6 @@ class MathAIClient:
             logger.error(f"LLM ошибка при запросе: {e}")
             raise
 
+    #Закрытие сессии
     async def close(self):
         await self.client.aclose()
